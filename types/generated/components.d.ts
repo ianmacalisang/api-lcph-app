1f378e8c966744cfb33a5d8235ee5602c086fb29
ian andrew macalisang <dev@ianmacalisang.com>
2024-10-29 08:39:02 +0800

    promotion duration updated

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -38,7 +38,7 @@ export interface FrontendPromotions extends Schema.Component {
       'oneToMany',
       'api::download.download'
     >;
-    subscription: Attribute.Date;
+    duration: Attribute.Integer & Attribute.DefaultTo<12>;
   };
 }
 
